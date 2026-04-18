eae8cd68a0418af0781b220b85c306dfbc7adda6
lapinouche <ammiratiflorent5406@gmail.com>
2026-04-18 18:59:20 +0200

    Update obstacle_avoidance.py

--- projet/projet/obstacle_avoidance.py
+++ projet/projet/obstacle_avoidance.py
@@ -10,114 +10,128 @@ class ObstacleAvoidanceNode(Node):
         super().__init__('obstacle_avoidance')
         
         # Subscriber pour l'image du projet 2025
-        self.subscription = self.create_subscription(
+        self.subscription_img = self.create_subscription(
             CompressedImage, 
             '/image_raw/compressed', # Make sure the topic name is correct!
             self.camera_callback, 
             10)
+        
+        # Create the subscriber for LIDAR data
+        self.subscription_lidar = self.create_subscription(
+            LaserScan,
+            '/scan',           # The default topic for TurtleBot LiDAR
+            self.scan_callback,
+            10                 # QoS history depth
+        )
 
-        # 3. Talk to the actual robot
+        # Publisher to cmd_vel_obstacle
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
 
-
     def camera_callback(self, msg):
-        # 1. Décodage de l'image
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        if image is None: return
+        # 1. Avoid cylinder
+        max_range = 0.8 # Meters
+        min_range = 0.3 # was 0.1
 
-        height, width, _ = image.shape
-        # On regarde 60% de l'image pour esquiver les plots
-        roi = image[int(height*0.3):height, :]
-        roi_height = roi.shape[0] # Get the height of the cropped image
-        #roi = image[int(height*0.4):int(height*0.8), :] # to get a better view
-        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        # look in front of the robot to avoid obstacle
+        front_left_side = msg.ranges[0:60] 
+        valid_front_left = [l for l in front_left_side if min_range < l < max_range]
 
-        # 2. Masques de couleur (Cylindre) 
-        # Ajustez ces seuils si les couleurs sur le terrain réel diffèrent
-        lower_blue = np.array([100, 150, 50]) 
-        upper_blue = np.array([130, 255, 255])
-        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-        # filtrage de la couleur bleu
-        kernel = np.ones((5, 5), np.uint8)
-        mask_blue = cv2.morphologyEx(mask_blue, cv2.MORPH_CLOSE, kernel) # This fills small holes inside the object
-        mask_blue = cv2.morphologyEx(mask_blue, cv2.MORPH_OPEN, kernel) # Optional: This removes tiny speckles of noise
-        mask_blue = cv2.dilate(mask_blue, kernel, iterations=1) # This makes the blue object appear thicker to the algorithm
+        front_right_side = msg.ranges[300:360]
+        valid_front_right = [r for r in front_right_side if min_range < r < max_range]
 
-        # 1. Find all blue shapes
-        contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # look at the direct left and right to the robot (if object, don't turn yet)
+        left_side = msg.ranges[80:100] 
+        valid_left = [l for l in left_side if min_range < l < max_range]
+
+        right_side = msg.ranges[260:280]
+        valid_right = [r for r in right_side if min_range < r < max_range]
 
         is_obstacle_detected = False
-
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > 380:  # Only look at significant objects
-                x, y, w, h = cv2.boundingRect(cnt)
-                
-                # 2. Shape check: Cylinders are usually taller than they are wide 
-                # or at least not "flat" like tape.
-                aspect_ratio = float(h) / w
-                
-                # Tape on floor usually has h < w (aspect ratio < 1)
-                # Cylinder usually has h > w (aspect ratio > 1)
-                if aspect_ratio > 0.75: # 0.7 / 0.8
-                    is_obstacle_detected = True
-                    
-                    # Use moments for THIS specific contour
-                    m = cv2.moments(cnt)
-                    cx = int(m['m10'] / m['m00'])
-                    
-                    twist = Twist()
-                    if cx < 320:
-                        twist.angular.z = -0.7 # Turn Right (0.5)
-                        twist.linear.x = 0.05  # Slow down while avoiding
-                    
-                    elif cx > 340:
-                        twist.angular.z = 0.7  # Turn Left
-                        twist.linear.x = 0.05
-                    
-                    else:
-                        twist.angular.z = 0.8  # Turn Left
-                        twist.linear.x = 0.02
-                    
-                    self.cmd_vel_pub.publish(twist)
-                    break # Exit the loop, we found our obstacle!
-
-        # 3. If no standing cylinders were found, follow the line
-        if not is_obstacle_detected:
-            self.cmd_vel_pub.publish(Twist())
-
-        """
-        bc = cv2.moments(mask_blue)
         twist = Twist()
 
-        # 4. Logique de suivi (Rester entre les deux lignes) 
-        if bc['m00'] > 500:  # (500 = nuber of pixel to make the robot turn) -> Threshold: only react if the blue object is big enough
-            # Calculate the center of the blue object
-            cx = int(bc['m10'] / bc['m00'])
-            
-            # Get the center of the camera frame (assuming 640px wide)
-            screen_center = 320 
-            
-            if cx < screen_center:
-                # Obstacle is on the LEFT -> Turn RIGHT
-                #print("Blue on Left! Steering Right.")
-                twist.angular.z = -0.5
-            
+        if valid_front_left:
+            if len(valid_left) != 0:
+                dist_left = sum(valid_front_left) / len(valid_front_left)
+                self.get_logger().info(f"dist_front_left : {dist_left}")
+                is_obstacle_detected = True
             else:
-                # Obstacle is on the RIGHT -> Turn LEFT
-                #print("Blue on Right! Steering Left.")
-                twist.angular.z = 0.5
+                pass
 
-            # Publish when we are actually dodging
+            twist.angular.z = -1 # (0.7) Turn Right
+            twist.linear.x = 0.03  # Slow down while avoiding
+
             self.cmd_vel_pub.publish(twist)
-            
-        else:
-            # NO OBSTACLE: Just repeat what the line follower said
-            self.cmd_vel_pub.publish(self.latest_line_twist)
-        """
+            break
+        
+        elif valid_left:
+            if len(valid_left) != 0:
+                dist_left = sum(valid_left) / len(valid_left)
+                self.get_logger().info(f"dist_left : {dist_left}")
+                is_obstacle_detected = True
+            else:
+                pass
 
-        cv2.imshow("Masque Bleu", mask_blue)
+            if dist_left > 3:
+                is_obstacle_detected = False # Safe distance, try to keep between the line
+                twist.linear.x = 0.07  # Keep going
+            else:
+                twist.angular.z = -0.3 # turn a little more
+                twist.linear.x = 0.04  # Slow down !
+
+            self.cmd_vel_pub.publish(twist)
+            break
+
+            
+        elif valid_front_right:
+            if len(valid_right) != 0:
+                dist_right = sum(valid_front_right) / len(valid_front_right)
+                self.get_logger().info(f"dist_front_right : {dist_right}")
+                is_obstacle_detected = True
+            else:
+                pass
+
+            twist.angular.z = 1 # (0.7) Turn Left
+            twist.linear.x = 0.03  # Slow down while avoiding
+
+            self.cmd_vel_pub.publish(twist)
+            break
+
+        elif valid_right
+            if len(valid_right) != 0:
+                dist_right = sum(valid_right) / len(valid_right)
+                self.get_logger().info(f"dist_left : {dist_right}")
+                is_obstacle_detected = True
+            else:
+                pass
+
+            if dist_right > 3:
+                is_obstacle_detected = False # Safe distance, try to keep between the line
+                twist.linear.x = 0.07  # Keep going
+            else:
+                twist.angular.z = -0.3 # turn a little more
+                twist.linear.x = 0.04  # Slow down !
+
+            self.cmd_vel_pub.publish(twist)
+            break
+                
+        # 3. If no standing cylinders were found, keep following the line
+        if not is_obstacle_detected:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            # 1. Décodage de l'image
+            
+            # 2. Masques de couleur (line_following)
+            # Ajustez ces seuils si les couleurs sur le terrain réel diffèrent
+
+            #####
+            ## TO BE DONE (or to be delet if using linear.x = 0 and angular.z = 0)
+            #####
+
+            #twist.linear.x = 0.0
+            #twist.angular.z = 0.0
+            self.cmd_vel_pub.publish(twist)
+
+        #cv2.imshow("Masque Bleu", mask_blue)
         cv2.waitKey(1)
         
 
